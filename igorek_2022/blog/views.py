c35122dfdd5eeadc7f5475e0059c1b850bfc7da7
epame <epam.rb@gmail.com>
2023-01-25 21:00:39 +0300

    Kommentarii

--- igorek_2022/blog/views.py
+++ igorek_2022/blog/views.py
@@ -26,7 +26,7 @@ def blog_detail(request, pk):
             comment.save()
     comments = Comment.objects.filter(post=post)
     context = {
-        #"post": post,
+        "post": post,
         "comments": comments,
         "form": form,
     }
@@ -38,6 +38,6 @@ def blog_category(request, category):
     posts = Post.objects.filter(categories__name__contains=category).order_by('-created_on')
     context = {
         "category": category,
-        #"posts": posts,
+        "posts": posts,
     }
     return render(request, "blog_category.html", context)
